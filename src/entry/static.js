656b9083d639b9a58024977233d0938276177e81
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-02-28 20:40:56 +0100

    Share image, og meta tags

--- src/entry/static.js
+++ src/entry/static.js
@@ -3,6 +3,8 @@ import ReactDOMServer from 'react-dom/server';
 
 import Page from '../page';
 
+const shareImageUrl = require('../../static/share-image.jpg')
+
 module.exports = function(locals) {
     const cssFiles = [];
     locals.webpackStats.compilation.chunks.forEach(chunk => {
@@ -19,13 +21,26 @@ module.exports = function(locals) {
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-                <title>Zurihac 2017</title>
+                <title>ZuriHac 2017</title>
 
                 <link rel="shortcut icon" href={require('../../static/favicon.ico')} />
                 <link href="https://fonts.googleapis.com/css?family=Lora|Raleway:400,500" rel="stylesheet" />
                 <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.31.0/mapbox-gl.css' rel='stylesheet' />
 
                 <link href={'https://zurihac.info/' + cssFiles[0]} rel='stylesheet' />
+
+                <meta name="twitter:card"        content="summary_large_image" />
+                <meta name="twitter:site"        content="@zurihac" />
+                <meta name="twitter:creator"     content="@wereHamster" />
+                <meta name="twitter:title"       content="ZuriHac 2017" />
+                <meta name="twitter:image"       content={shareImageUrl} />
+                <meta name="twitter:description" ontent="The Haskell Hackathon is an international, grassroots collaborative coding festival whose goal is to expand the community and to build and improve Haskell libraries, tools, and infrastructure." />
+
+                <meta property="og:url"          content="https://zurihac.info"/>
+                <meta property="og:type"         content="article" />
+                <meta property="og:title"        content="ZuriHac 2017" />
+                <meta property="og:image"        content={shareImageUrl} />
+                <meta property="og:description"  content="The Haskell Hackathon is an international, grassroots collaborative coding festival whose goal is to expand the community and to build and improve Haskell libraries, tools, and infrastructure." />
             </head>
             <body>
                 <Page />
