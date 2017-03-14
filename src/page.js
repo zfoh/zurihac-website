14a689d3c7a4ece6dacf8fffbc6f3f8441b516b3
Gleb Peregud <gleber.p@gmail.com>
2017-03-14 22:19:07 +0100

    Add 'Terms and Conditions' section

--- src/page.js
+++ src/page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Header, About, Sponsors, Venue, Media, Registration, Contact, Footer} from './sections';
+import {Header, About, Sponsors, Venue, Media, Registration, Contact, Conditions, Footer} from './sections';
 import Projects from './projects'
 import Schedule from './schedule';
 import Speakers from './speakers';
@@ -18,6 +18,7 @@ export default () => (
         <Media />
         <Registration />
         <Contact />
+        <Conditions />
         <Footer />
     </div>
 );
