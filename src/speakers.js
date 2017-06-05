23431a25c2a9908e7c3b08216144a53472be27e4
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-06-05 19:20:07 +0200

    Mention speaker time and place

--- src/speakers.js
+++ src/speakers.js
@@ -26,6 +26,9 @@ const Speaker = ({img, title, author, github, twitter, children}) => (
 );
 
 
+const TimeAndPlace = ({children}) => (
+    <div className={styles.speakerTimeAndPlace}>{children}</div>
+);
 
 
 export default () => (
@@ -35,17 +38,20 @@ export default () => (
 
         <div className={styles.talksSection}>
             <Speaker author="Edward Kmett" img={require('../static/ekmett.jpg')} github='ekmett' twitter='kmett'>
+                <TimeAndPlace>Keynote on Sunday 10:00</TimeAndPlace>
                 Prolific Haskell programmer, mathematician, lapsed graphics guru and demo scener, defense contractor, financial toolsmith,
                 author of the widely-known <a href="https://github.com/ekmett/lens/">lens</a> library.
             </Speaker>
 
             <Speaker author="Stephen Diehl" img={require('../static/stephen-diehl.jpg')} github='sdiehl' twitter='smdiehl'>
+                <TimeAndPlace>Keynote on Saturday 10:00</TimeAndPlace>
                 Stephen is a Haskell developer, entrepreneur, and financial systems architect from Boston. His open source
                 work is around numerical computing, compilers, security, and improving Haskell documentation.
                 Author of the amazing <a href="http://dev.stephendiehl.com/hask/">What I Wish I Knew When Learning Haskell</a>.
             </Speaker>
 
             <Speaker author="Julie Moronuki" img={require('../static/julie.jpg')} github='GinBaby' twitter='argumatronic'>
+                <TimeAndPlace>Keynote on Friday 17:00</TimeAndPlace>
                 Julie is a linguist and teacher who decided, on a whim, to learn Haskell. As her interest in lambdas and
                 static typing grew, she decided to use her teaching expertise to write a Haskell book for beginners,
                 and the result was <a href='http://haskellbook.com/'>Haskell Programming from First Principles</a>.
@@ -54,6 +60,7 @@ export default () => (
             </Speaker>
 
             <Speaker author="Neil Mitchell" img={require('../static/ndmitchell.jpg')} github='ndmitchell' twitter='ndm_haskell'>
+                <TimeAndPlace>Keynote on Friday 10:00</TimeAndPlace>
                 Neil Mitchell has been a Haskell programmer since his PhD at York
                 University, where he worked on making functional programs shorter,
                 faster and safer. Since then he's worked in industry, taking the
@@ -63,6 +70,7 @@ export default () => (
             </Speaker>
 
             <Speaker author="Simon Thompson" img={require('../static/simon-thompson.jpg')} twitter='thompson_si'>
+                <TimeAndPlace>Keynote on Saturday 17:00</TimeAndPlace>
                 Simon Thompson is Professor of Logic and Computation in the
                 School of Computing at
                 the <a href="https://www.cs.kent.ac.uk/people/staff/sjt/">University of Kent</a>.
