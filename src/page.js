e91be1f434c480f7e2a620a5e3be6c7bea9888f4
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-06-07 05:45:39 +0200

    Remove registration section

--- src/page.js
+++ src/page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Header, About, Sponsors, Venue, Beginners, Media, Registration, Contact, Conditions, Footer, Food} from './sections';
+import {Header, About, Sponsors, Venue, Beginners, Media, Contact, Conditions, Footer, Food} from './sections';
 import Projects from './projects'
 import Schedule from './schedule';
 import Speakers from './speakers';
@@ -18,7 +18,6 @@ export default () => (
         <Beginners />
         <Venue />
         <Media />
-        <Registration />
         <Contact />
         <Conditions />
         <Footer />
